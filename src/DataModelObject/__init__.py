b4b62940aa30777f868ec0a3b32064267b24d95c
허용선 <yongseon.heo@hugraph.com>
2024-07-24 19:00:01 +0900

    모델 데코레이터, 추상 클래스 추가

--- src/DataModelObject/__init__.py
+++ src/DataModelObject/__init__.py
@@ -4,8 +4,13 @@ from .DataModelObject import DataModelObject
 from .DataAttributeObject import DataAttributeObject
 from .DataModelObjectHandler import DataModelObjectHandler
 
+from .DataModelObjectExecutor import DataModelObjectExecutor
+from .DataModelObjectFactory import DataModelObjectFactory
+
 __all__ = (
 	'DataModelObjectHandler',
 	'DataModelObject',
 	'DataAttributeObject',
+	'DataModelObjectExecutor',
+	'DataModelObjectFactory',
 )
