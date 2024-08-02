08e10ccb0861a5ce1a602d3248c125ac34c1d642
허용선 <yongseon.heo@gmail.com>
2024-08-03 01:15:00 +0900

    실행 가능 인터페이스 추가

---
+++ src/DataAccessObject/Model/Executable.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+from .Executors import Executors
+from .Executor import Executor
+
+from ...DataModel import Model
+
+from typing import Union
+
+__all__ = (
+	'Executable'
+)
+
+
+class Executable(metaclass=ABCMeta):
+	"""Executable Interface for Model"""
+	@abstractmethod
+	def runs(self, executors: Executors) -> None:
+		raise NotImplementedError('{} must be implemented runs'.format(self.__class__.__name__))
+	
+	@abstractmethod
+	def run(self, executor: Executor) -> Union[Model, list[Model], list]:
+		raise NotImplementedError('{} must be implemented run'.format(self.__class__.__name__))
