0bad0c363ab020451d7b19a4957141941ae17324
허용선 <yongseon.heo@gmail.com>
2024-08-09 02:26:37 +0900

    불필요한 참조 제거

--- src/DataAccessObject/Error.py
+++ src/DataAccessObject/Error.py
@@ -12,21 +12,11 @@ class Error(BaseException):
 
 	def __init__(self, reason, error=None):
 		super(Error, self).__init__(reason)
-		self.reason = reason
 		self.error = error
 		return
-
-	def __repr__(self):
-		return self.__str__()
-
-	def __str__(self):
-		reason = ''
-		if self.reason:
-			reason += self.reason + '\n'
+	
+	@property
+	def __traceback__(self):
 		if self.error:
-			for line in ''.join(traceback.format_tb(self.error.__traceback__)).strip().split('\n'):
-				reason += line + '\n'
-		else:
-			for line in ''.join(traceback.format_tb(self.__traceback__)).strip().split('\n'):
-				reason += line + '\n'
-		return reason
+			return self.error.__traceback__
+		return super(Error, self).__traceback__
