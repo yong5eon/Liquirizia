d933dd64c848aeaf85fcc3b048d69880c673febc
허용선 <yongseon.heo@gmail.com>
2024-08-09 02:26:11 +0900

    Properties 를 Types 로 변경

---
+++ src/DataAccessObject/Types/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from .Database import Database
+from .Document import Document
+from .Index import Index
+from .Cache import Cache
+
+__all__ = (
+	'Database',
+	'Document',
+	'Index',
+	'Cache',
+)
