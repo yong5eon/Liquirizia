5607359fe3f9ae9d834e619311c6b2a48bf8be73
허용선 <yongseon.heo@gmail.com>
2024-08-09 02:26:51 +0900

    PrettyPrintSQL 제거

--- src/Util/__init__.py
+++ src/Util/__init__.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from pprint import PrettyPrinter
 from sys import stdout
-from re import search, IGNORECASE
 
 __all__ = (
 	'PrettyPrint',
 	'PrettyDump',
-	'PrettyPrintSQL',
 )
 
 
@@ -80,72 +77,3 @@ def PrettyDump(obj, indent=2):
 				lf = True
 	__ += LF
 	return __
-
-
-def PrettyPrintSQL(sql, indent=2, file=stdout):
-	step = 0
-	offset = 0
-	CR = '\r'
-	LF = '\n'
-	BLANK = ' '
-	PATTERNS_PRE_LF = [
-		'FROM',
-		'INNER JOIN',
-		'LEFT OUTER JOIN',
-		'LEFT JOIN',
-		'RIGHT OUTER JOIN ',
-		'RIGHT JOIN ',
-		'WHERE',
-		'ORDER BY',
-		'GROUP BY',
-		'HAVING',
-		'LIMIT',
-	]
-	PATTERNS_POST_LF = [
-		'SELECT',
-		'FROM',
-		'INNER JOIN',
-		'LEFT OUTER JOIN',
-		'LEFT JOIN',
-		'RIGHT OUTER JOIN ',
-		'RIGHT JOIN ',
-		'WHERE',
-		'ORDER BY',
-		'GROUP BY',
-		'HAVING',
-		'LIMIT',
-		','
-	]
-	PATTERNS_PRE_LF_NONE_STEP = [
-		'INNER JOIN',
-		'LEFT OUTER JOIN',
-		'LEFT JOIN',
-		'RIGHT OUTER JOIN ',
-		'RIGHT JOIN ',
-	]
-	PATTERNS_POST_LF_NONE_STEP = [',']
-	PATTERNS = set(PATTERNS_PRE_LF + PATTERNS_POST_LF)
-	PATTERNS_REGEX = '|'.join(PATTERNS)
-	while offset < len(sql):
-		m = search(PATTERNS_REGEX, sql[offset:])
-		if not m:
-			break
-		file.write(BLANK * (step * indent))
-		expr = sql[offset:offset+m.span()[0]].strip()
-		if len(expr):
-			file.write(expr)
-		token = sql[offset+m.span()[0]:offset+m.span()[1]]
-		if token.upper() in PATTERNS_PRE_LF:
-			file.write(LF)
-			step -= 1
-			file.write(BLANK * (step * indent))
-		file.write(token)
-		if token.upper() in PATTERNS_POST_LF:
-			file.write(LF)
-			if token not in PATTERNS_POST_LF_NONE_STEP:
-				step += 1
-		offset += m.span()[1]
-	file.write(BLANK * (step * indent))
-	file.write(sql[offset:].strip())
-	file.write(LF)
-	return
