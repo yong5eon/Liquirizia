364d169f8529344519459f9585a48cba078bddcd
허용선 <yongseon.heo@gmail.com>
2024-09-01 23:17:25 +0900

    데이터 모델 테스트 추가

--- test/DataModel.py
+++ test/DataModel.py
@@ -11,81 +11,160 @@ from Liquirizia.DataModel import (
 from Liquirizia.Validator import Validator, Pattern
 from Liquirizia.Validator.Patterns import *
 
-from random import random, randrange
-from sys import stderr
-
-
-class DataModelPropertiesHandler(Handler):
-	def __call__(self, o : Model, n : str, v : any, p : any):
-		return
-
-
-class DataModelProperties(Model):
-	typeInteger = Attribute(Validator(IsInteger(IsRange(0, 10, 3))), fn=DataModelPropertiesHandler())
-	typeFloat = Attribute(Validator(IsFloat(IsRange(1, 10, 3))), fn=DataModelPropertiesHandler())
-
-
-class DataHandler(Handler):
-	def __call__(self, o : Model, n : str, v : any, p : any):
-		return
-
-
-class IsDataModelProperties(Pattern):
-	def __call__(self, parameter):
-		if not isinstance(parameter, DataModelProperties):
-			raise TypeError('{} is not Model'.format(parameter.__class__.__name__))
-		return parameter
-
-
-class DataModel(Model):
-	typeBool = Attribute(Validator(IsBool(IsEqualTo(True))), fn=DataHandler())
-	typeInteger = Attribute(Validator(IsInteger(IsRange(0, 10, 2))), fn=DataHandler())
-	typeFloat = Attribute(Validator(IsFloat(IsRange(1, 10, 2))), fn=DataHandler())
-	typeString = Attribute(Validator(IsString(IsIn('Hello', 'Hi'))), fn=DataHandler())
-	typeList = Attribute(Validator(IsList(IsElementOf(IsInteger(IsRange(0, 10))))), fn=DataHandler())
-	typeListOfList = Attribute(Validator(IsAbleToNone(IsList(IsElementOf(IsList(IsElementOf(IsRange(0, 10))))))), fn=DataHandler())
-	typeListOfDict = Attribute(
-		Validator(IsAbleToNone(IsList(IsElementOf(IsDictionary(
-			IsRequiredIn('typeInteger', 'typeFloat'),
-			IsMappingOf({
-				'typeInteger': Validator(IsInteger(IsIn(1,2,3))),
-				'typeFloat': Validator(IsFloat(IsRange(0, 5))),
-				'typeList': Validator(IsList(IsElementOf(IsIn('KIM', 'LEE', 'PARK', 'CHOI', 'HEO'))))
-			})
-		))))),
-		fn=DataHandler()
-	)
-	typeDict = Attribute(Validator(IsDictionary(
-		IsMappingOf({
-			'typeList': Validator(IsList(IsElementOf(IsIn('KIM', 'BANG', 'JEONG', 'HEO'))))
-		})
-	)), fn=DataHandler())
-	typeDataModel = Attribute(Validator(IsDataModelProperties()), fn=DataHandler())
-	typeListOfDataModel = Attribute(Validator(IsList(IsElementOf(IsDataModelProperties()))), fn=DataHandler())
+from copy import copy, deepcopy
 
 
 class TestDataModel(Case):
 
-	@Order(0)
-	def test_init(self):
-		self._ = DataModel(
-			typeBool=True,
-			typeInteger=0,
-			typeFloat=1.0,
-			typeString='Hello',
-			typeList=[],
-			typeDict={
-				'typeList': ['KIM',]
-			},
-			typeListOfDict=[],
-			typeDataModel=DataModelProperties(
-				typeInteger=3,
-				typeFloat=4.0
-			),
-			typeListOfDataModel = [
-				DataModelProperties(
-					typeInteger=3,
-					typeFloat=7.0
-				)
-			]
-		)
+	@Parameterized(
+			{'v': True},
+			{'v': False},
+	)
+	@Order(1)
+	def testBool(self, v):
+		class TestModel(Model):
+			v = Attribute(Validator(IsAbleToNone(IsBool())))
+		_ = TestModel()
+		ASSERT_IS_NOT_INSTANCE(_.v, bool)
+		ASSERT_IS_NONE(_.v)
+		_.v = v
+		ASSERT_IS_INSTANCE(_.v, bool)
+		ASSERT_IS_NOT_NONE(_.v)
+		ASSERT_IS_EQUAL(_.v, v)
+
+	@Parameterized(
+			{'v': 0},
+			{'v': 1},
+			{'v': 2},
+			{'v': 3},
+			{'v': 4},
+			{'v': 5},
+	)
+	@Order(2)
+	def testInteger(self, v):
+		class TestModel(Model):
+			v = Attribute(Validator(IsAbleToNone(IsInteger())))
+		_ = TestModel()
+		ASSERT_IS_NOT_INSTANCE(_.v, int)
+		ASSERT_IS_NONE(_.v)
+		_.v = v
+		ASSERT_IS_INSTANCE(_.v, int)
+		ASSERT_IS_NOT_NONE(_.v)
+		ASSERT_IS_EQUAL(_.v, v)
+
+	@Parameterized(
+			{'v': 0.},
+			{'v': 1.},
+			{'v': 2.},
+			{'v': 3.},
+			{'v': 4.},
+			{'v': 5.},
+	)
+	@Order(3)
+	def testFloat(self, v):
+		class TestModel(Model):
+			v = Attribute(Validator(IsAbleToNone(IsFloat())))
+		_ = TestModel()
+		ASSERT_IS_NOT_INSTANCE(_.v, float)
+		ASSERT_IS_NONE(_.v)
+		_.v = v
+		ASSERT_IS_INSTANCE(_.v, float)
+		ASSERT_IS_NOT_NONE(_.v)
+		ASSERT_IS_EQUAL(_.v, v)
+
+	@Parameterized(
+			{'v': 'Hello'},
+			{'v': 'Hi'},
+	)
+	@Order(4)
+	def testString(self, v):
+		class TestModel(Model):
+			v = Attribute(Validator(IsAbleToNone(IsString())))
+		_ = TestModel()
+		ASSERT_IS_NOT_INSTANCE(_.v, str)
+		ASSERT_IS_NONE(_.v)
+		_.v = v
+		ASSERT_IS_INSTANCE(_.v, str)
+		ASSERT_IS_NOT_NONE(_.v)
+		ASSERT_IS_EQUAL(_.v, v)
+
+	@Parameterized(
+			{'v': [True, False]},
+			{'v': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]},
+			{'v': [0., 1., 2., 3., 4., 5., 6., 7., 8., 9.]},
+			{'v': ['Hello', 'World', 'Nice', 'to', 'meet', 'you']},
+			{'v': [True, 0, 0., 1, 1.0, 'Hello', 'Hi']},
+			{'v': [(1,), (2,)]},
+			{'v': [[1,], [2,]]},
+			{'v': [{'a': 1, 'b': 2},{}]},
+	)
+	@Order(4)
+	def testList(self, v):
+		class TestModel(Model):
+			v = Attribute(Validator(IsAbleToNone(IsListable())))
+		_ = TestModel()
+		ASSERT_IS_NONE(_.v)
+		l = []
+		_.v = []
+		ASSERT_IS_NOT_NONE(_.v)
+		for n in v:
+			l.append(n)	
+			_.v.append(n)
+		ASSERT_IS_NOT_NONE(_.v)
+		for i, n in enumerate(l):
+			if isinstance(n, (list, tuple)):
+				ASSERT_IS_EQUAL(n, copy(_.v[i]))
+				continue
+			if isinstance(n, dict):
+				ASSERT_IS_EQUAL(n, copy(_.v[i]))
+			if isinstance(n, Model): continue
+			ASSERT_IS_EQUAL(n, _.v[i])
+
+	@Parameterized(
+			{'v': {}},
+			{'v': {'a': 1, 'b': 0.1, 'c': 'Hi', 'd': True, 'e': [1], 'f': (1,), 'g': {'a': 1, 'b': 0.3}}},
+	)
+	@Order(4)
+	def testDict(self, v):
+		class TestModel(Model):
+			v = Attribute(Validator(IsAbleToNone(IsDictionary())))
+		_ = TestModel()
+		ASSERT_IS_NONE(_.v)
+		o = {}
+		_.v = {}
+		ASSERT_IS_NOT_NONE(_.v)
+		for k, v in v.items():
+			o[k] = v
+			_.v[k] = v
+		ASSERT_IS_NOT_NONE(_.v)
+		for k, v in o.items():
+			if isinstance(v, (list, tuple)):
+				ASSERT_IS_EQUAL(v, copy(_.v[k]))
+				continue
+			if isinstance(v, dict):
+				ASSERT_IS_EQUAL(v, copy(_.v[k]))
+			if isinstance(v, Model): continue
+			ASSERT_IS_EQUAL(v, _.v[k])
+
+	@Parameterized(
+	)
+	@Order(5)
+	def testHandler(self, v, const):
+		class TestHandler(Handler):
+			def __init__(self, v):
+				self.v = v
+				return
+			def __call__(self, model: any, attr: any, value: any, previousValue: any):
+				model.b = model.a + self.v
+				return
+		class TestModel(Model):
+			a = Attribute(Validator(IsAbleToNone(IsInteger())), fn=TestHandler(const))
+			b = Attribute(Validator(IsAbleToNone(IsInteger())))
+			_ = TestModel()
+			ASSERT_IS_NONE(_.a)
+			ASSERT_IS_NONE(_.b)
+			_.a = v
+			ASSERT_IS_EQUAL(v, _.a)
+			ASSERT_IS_EQUAL(v + const, _.b)
+		
+
