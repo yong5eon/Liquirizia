9593fbf50e2e24c4f81b9c8c785a44ac0787f668
허용선 <yongseon.heo@gmail.com>
2024-09-03 01:57:23 +0900

    패치가능 인터페이스 추가

--- src/DataAccessObject/Model/__init__.py
+++ src/DataAccessObject/Model/__init__.py
@@ -6,10 +6,12 @@ from .Executor import Executor
 from .Executors import Executors
 
 from .Executable import Executable
+from .Fetchable import Fetchable
 
 __all__ = (
 	'Type',
 	'Executor',
 	'Executors',
 	'Executable',
+	'Fetchable',
 )
