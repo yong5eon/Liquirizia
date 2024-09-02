07be973c3e9929843686f7d14527958a6d766654
허용선 <yongseon.heo@gmail.com>
2024-09-03 01:56:31 +0900

    실행 에러 클래스 변경

--- src/DataAccessObject/Types/Database/Errors/ExecuteError.py
+++ src/DataAccessObject/Types/Database/Errors/ExecuteError.py
@@ -12,19 +12,6 @@ __all__ = (
 class ExecuteError(Error):
 	"""Execute Error Class"""
 
-	def __init__(self, reason='Execution is error', error=None, code=None, sql=None, args=None):
+	def __init__(self, reason='Execution is error', error=None):
 		super(ExecuteError, self).__init__(reason, error)
-		self.code = code
-		self.sql = sql
-		self.args = args
 		return
-
-	def __str__(self):
-		reason = super(ExecuteError, self).__str__()
-		if self.code:
-			reason += self.code + '\n'
-		if self.sql:
-			reason += self.sql + '\n'
-		if self.args:
-			reason += str(self.args)+ '\n'
-		return reason
