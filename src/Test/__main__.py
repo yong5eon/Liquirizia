3b62f0fd313a866cdb0c6272e54addd4e940d044
허용선 <yongseon.heo@gmail.com>
2024-10-30 22:47:25 +0900

    특정 테스트 파일을 불러 테스트 할수 있도록 기능 추가

--- src/Test/__main__.py
+++ src/Test/__main__.py
@@ -3,33 +3,46 @@
 from .Loader import Loader
 from .Runner import Runner
 
-from unittest import TestLoader, TestProgram
-from sys import argv
 from argparse import ArgumentParser
+
+from sys import stderr
 
 parser = ArgumentParser(prog='Liquirizia Test Runner')
 
-parser.add_argument('path', default='.', help="Path to test('.' default)")
-parser.add_argument('-v', '--verbose', dest='verbosity', action='store', default=2, help='Verbose output(default=2)')
-parser.add_argument('-f', '--failfast', dest='failfast', action='store_true', help='Stop on first fail or error')
-parser.add_argument('-s', '--start', dest='start', default='.', help='Start base directory of test(default=., start directory)')
-parser.add_argument('-p', '--pattern', dest='pattern', default='*.py', help='Pattern of test file(default=*.py)')
+parser.add_argument('path', help="File or Module to test")
 
+parser.add_argument('-n', '--pattern', dest='pattern', default='*.py', help='Pattern of test file(default=*.py)')
+parser.add_argument('-t', '--base', dest='base', help='Top level directory of test from path')
+
+parser.add_argument('-v', '--verbose', dest='verbosity', action='store', default=2, help='Verbose output(default=2)')
+parser.add_argument('-s', '--failfast', dest='failfast', action='store_true', help='Stop on first fail or error')
 
 args = parser.parse_args()
 
-def cmp(a, b):
-	return a
+if not args.path:
+	parser.print_help(file=stderr)
+	exit(-1)
 
 # load tests
 loader = Loader()
-tests =  loader.discover(args.path, top_level_dir=args.start, pattern=args.pattern)
+
+tests = None
+
+try:
+	tests =  loader.file(args.path)
+except Exception as e:
+	tests =  loader.discover(args.path, pattern=args.pattern, top_level_dir=args.base)
+finally:
+	if not tests:
+		print('No Test Found\n', file=stderr)
+		parser.print_help(file=stderr)
+		exit(1)
 
 # run tests
 runner = Runner(verbosity=args.verbosity, failfast=args.failfast)
 result = runner.run(tests)
 
-if len(result.failures): exit(1)
-if len(result.errors): exit(-1)
+if len(result.failures): exit(-1)
+if len(result.errors): exit(-2)
 
 exit(0)
