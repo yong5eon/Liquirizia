c8bb38c626eb6c174f482a2adfa9f1343306de84
허용선 <yongseon.heo@gmail.com>
2024-11-16 20:42:34 +0900

    WINDOWS 외의 다른 플랫폼에서는 SIGNAL 을 사용하도록 변경

--- src/System/Util/Timer.py
+++ src/System/Util/Timer.py
@@ -1,28 +1,62 @@
 # -*- coding: utf-8 -*-
 
-from threading import Timer as ThreadTimer
+from platform import system
+
 
 from typing import Callable
 
 __all__ = (
-	'SetTimer'
+	'SetTimer',
+	'Timeout',
 )
 
 
 class Timer(object):
-	def __init__(self, seconds: float, cb: Callable):
-		self.timer = ThreadTimer(seconds, cb)
+	def __init__(self, ms: int, cb: Callable):
+		self.cb = cb
+		self.ms = ms
 		return
-	def start(self, *args, **kwds):
-		self.timer.start()
+	def start(self):
+		if system().upper() == 'WINDOWS':
+			from threading import Timer as ThreadTimer
+			self.timer = ThreadTimer(self.ms / 1000, self.cb)
+			self.timer.start()
+		else:
+			from signal import signal, SIGALRM, setitimer, ITIMER_REAL
+			def callback(sig, frame):
+				return self.cb()
+			self.timer = signal(SIGALRM, callback)
+			setitimer(ITIMER_REAL, self.ms / 1000, 0.0)	
 		return
 	def stop(self):
-		self.timer.cancel()
+		if system().upper() == 'WINDOWS':
+			self.timer.cancel()
+		else:
+			from signal import signal, SIGALRM, setitimer, ITIMER_REAL
+			signal(SIGALRM, None)
 		return
 	
 
-def SetTimer(seconds: float, cb: Callable):
-	# TODO : in GNU/Linux, Mac OS, UNIX like, use signal with SIGALM OR SIGUSR
-	timer = Timer(seconds, cb)
+def SetTimer(ms: int, cb: Callable):
+	timer = Timer(ms, cb)
 	timer.start()
 	return timer
+
+
+class Timeout(object):
+	def __init__(
+		self,
+		timeout: int,
+		cb: Callable,
+	):
+		self.timeout = timeout
+		self.cb = cb
+		return
+	def __call__(self, fn: Callable):
+		def wrapper(*args, **kwargs):
+			timer = Timer(self.timeout, self.cb)
+			timer.start()
+			_ = fn(*args, **kwargs)
+			timer.stop()
+			return _
+		return wrapper
