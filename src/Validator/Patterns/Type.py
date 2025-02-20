09d5addafecb43a52ff9691d7e257137988489c9
허용선 <yongseon.heo@gmail.com>
2025-02-20 12:54:54 +0900

    IsDataObject 추가

--- src/Validator/Patterns/Type.py
+++ src/Validator/Patterns/Type.py
@@ -3,7 +3,7 @@
 from ..Pattern import Pattern
 
 from decimal import Decimal
-
+from dataclasses import is_dataclass
 from collections.abc import (
 	Iterable,
 	MutableSequence,
@@ -22,6 +22,7 @@ __all__ = (
 	'IsInteger',
 	'IsFloat',
 	'IsString',
+	'IsArray',
 	'IsList',
 	'IsTuple',
 	'IsSet',
@@ -31,6 +32,7 @@ __all__ = (
 	'IsByteArray',
 	'IsByteStream',
 	'IsDecimal',
+	'IsDataObject',
 	'ToTypeOf',
 	'ToBool',
 	'ToInteger',
@@ -43,7 +45,6 @@ __all__ = (
 	'ToByteArray',
 	'ToByteStream',
 	'ToDecimal',
-	'IsElementOf'
 )
 
 
@@ -90,84 +91,84 @@ class IsTypeOf(Pattern):
 
 
 class IsBool(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(bool, patterns=args, error=error)
 		return
 
 class IsInteger(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(int, patterns=args, error=error)
 		return
 
 
 class IsFloat(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(float, patterns=args, error=error)
 		return
 
 
 class IsString(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(str, patterns=args, error=error)
 		return
 
 
 class IsList(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(MutableSequence, patterns=args, error=error)
 		return
 
 
 class IsTuple(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(Sequence, patterns=args, error=error)
 		return
 
 
 class IsSet(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(MutableSet, patterns=args, error=error)
 		return
 
 
 class IsFixedSet(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(Set, patterns=args, error=error)
 		return
 
 
 class IsDictionary(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(MutableMapping, patterns=args, error=error)
 		return
 
 
 class IsMapping(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(Mapping, patterns=args, error=error)
 		return
 
 
 class IsByteArray(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(bytes, patterns=args, error=error)
 		return
 
 
 class IsByteStream(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(bytearray, patterns=args, error=error)
 		return
 	
 
 class IsDecimal(IsTypeOf):
-	def __init__(self, *args, error=None):
+	def __init__(self, *args, error: BaseException = None):
 		super().__init__(Decimal, patterns=args, error=error)
 		return
 
 
 class ToTypeOf(Pattern):
-	def __init__(self, type : type, error=None):
+	def __init__(self, type : type, error: BaseException = None):
 		self.type = type
 		self.error = error
 		return
@@ -184,69 +185,112 @@ class ToTypeOf(Pattern):
 		return '{}()'.format(self.type.__name__)
 	
 
-
 class ToBool(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(bool, error)
 		return
 
 
 class ToInteger(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(int, error)
 		return
 
 
 class ToFloat(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(float, error)
 		return
 
 
 class ToString(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(str, error)
 		return
 
 
 class ToList(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(list, error)
 		return
 
 
 class ToTuple(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(tuple, error)
 		return
 	
 
 class ToSet(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(set, error)
 		return
 
 
 class ToDictionary(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(dict, error)
 		return
 	
 
 class ToByteArray(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(bytes, error)
 		return
 
 
 class ToByteStream(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(bytearray, error)
 		return
 
 
 class ToDecimal(ToTypeOf):
-	def __init__(self, error=None):
+	def __init__(self, error: BaseException = None):
 		super().__init__(Decimal, error)
 		return
-	
+
+
+class IsDataObject(Pattern):
+	def __init__(
+		self, 
+		*patterns: Pattern,
+		error: BaseException = None
+	):
+		self.patterns = patterns if isinstance(patterns, Iterable) else (patterns,)
+		self.error = error
+		return
+
+	def __call__(self, parameter):
+		if not is_dataclass(parameter.__class__):
+			if self.error:
+				raise self.error
+			raise TypeError('{} must be dataclass'.format(
+				'\'{}\''.format(parameter) if isinstance(parameter, str) else parameter, 
+			))
+		for pattern in self.patterns:
+			parameter = pattern(parameter)
+		return parameter	
+
+
+class IsArray(Pattern):
+	def __init__(self, *args, error=None):
+		self.patterns = args
+		self.error = error
+		return
+
+	def __call__(self, parameter: Sequence) -> Sequence:
+		if not isinstance(parameter, Iterable):
+			if self.error:
+				raise self.error
+			raise TypeError('{} must be iterable'.format(parameter))
+		for pattern in self.patterns:
+			parameter = pattern(parameter)
+		return parameter
+
+	def __repr__(self):
+		return '{}({})'.format(
+			self.__class__.__name__,
+			', '.join([p.__repr__() for p in self.patterns])
+		)
+
