f4abb45eda19e722645e0e58580406984c52789b
허용선 <yongseon.heo@gmail.com>
2025-04-11 17:01:53 +0900

    TypeMapper, TypeEncoder 적용

--- src/Description/__init__.py
+++ src/Description/__init__.py
@@ -2,9 +2,12 @@
 
 from .Value import (
 	Value,
+	Condition,
 	Schema,
 	OneOf,
 	AllOf,
+	TypeMapper,
+	TypeEncoder,
 )
 from .Types import (
 	Boolean,
@@ -20,18 +23,25 @@ from .Types import (
 	Object,
 	Properties,
 )
-from .ToSchema import (
-	ToSchema,
-	DumpSchema,
-	SchemaToDataObject,
-	SchemaToDataModel,
+from .DataObject import (
+	DataObjectToSchema,
+	DataObjectToObject,
 )
 
+from dataclasses import is_dataclass
+from typing import Type, Dict
+from collections.abc import Sequence, Mapping
+from json import dumps, loads, JSONEncoder
+
 __all__ = (
+	# Value
 	'Value',
 	'Schema',
 	'OneOf',
 	'AllOf',
+	'TypeMapper',
+	'TypeEncoder',
+	# Types
 	'Boolean',
 	'IntegerFormat',
 	'Integer',
@@ -44,8 +54,64 @@ __all__ = (
 	'Array',
 	'Object',
 	'Properties',
+	# Utils
 	'ToSchema',
 	'DumpSchema',
-	'SchemaToDataObject',
-	'SchemaToDataModel',
+	# 'SchemaToDataObject',
+	'ToObject',
 )
+
+
+class SchemaEncoder(JSONEncoder):
+	"""Type Encoder for JSON"""
+	def default(self, obj):
+		if isinstance(obj, Sequence):
+			return list(obj)
+		if isinstance(obj, Mapping):
+			return dict(obj)
+		if isinstance(obj, Schema):
+			return obj.format
+		if isinstance(obj, Condition):
+			_ = []
+			for arg in obj.args:
+				if isinstance(arg, Schema):
+					_.append(arg.format)
+					continue
+				if isinstance(arg, Value):
+					_.append(arg)
+					continue
+				raise TypeError('{} is not supported'.format(arg.__class__.__name__))
+			return {'oneOf': _}
+		return None
+
+
+def DumpSchema(o: Schema):
+	def encode(schema: Schema):
+		return dumps(
+			schema,
+			cls=SchemaEncoder,
+			ensure_ascii=False,
+		)
+	if not isinstance(o, Schema):
+		raise TypeError('{} is not a Schema'.format(o.__name__))
+	return loads(encode(o))
+
+
+def SchemaToDataObject(o: Schema, name: str = None) -> object:
+	# TODO : make dataclass according to schema dynamically
+	raise NotImplementedError('SchemaToDataObject is not implemented yet')
+
+
+def ToSchema(o: Type[object], name: str = None, typedef: TypeMapper = None) -> Schema:
+	dataObjectToSchema = DataObjectToSchema(mapper=typedef)
+	if is_dataclass(o):
+		return dataObjectToSchema(o, name)
+	raise ValueError('{} cannot be converted to Schema'.format(o.__name__))
+
+
+def ToObject(o: object, typeenc: TypeEncoder = None) -> Dict:
+	# TODO : make dict object according to schema dynamically
+	dataObjectToObject = DataObjectToObject(encoder=typeenc)
+	if is_dataclass(o):
+		return dataObjectToObject(o)
+	raise ValueError('{} cannot be converted to Object'.format(o.__name__))
